3984c1471ae09636271c8c84c77e1347fd33cd57
CUzkov <UskovDanek@gmail.com>
2020-10-05 13:11:06 +0300

    Add struct

--- django-apps/users/users_app/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import url
-from .views import UserList
-
-
-urlpatterns = [
-    url('users/', UserList.as_view())
-]
--- django-apps/users/users_app/views.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import generics
-from django.contrib.auth import get_user_model
-from users_app.serializers import UserSerializer
-
-User = get_user_model()
-
-class UserList(generics.ListCreateAPIView):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
