e23499235a13a48709672373475edbaffe34f9e3
CUzkov <UskovDanek@gmail.com>
2020-11-23 18:30:13 +0300

    For fix backend

--- spa/src/types/user.ts
+++ spa/src/types/user.ts
@@ -10,14 +10,10 @@ export interface IUserStore {
 
 export interface IUserProfile {
 	id: string,
-	password: string,
 	last_login: string,
-	is_superuser: boolean,
 	username: string,
 	first_name: string,
 	last_name: string,
-	is_staff: boolean,
-	is_active: boolean,
 	date_joined: string,
 	birthday: string | null,
 	profile_image: string | null,
